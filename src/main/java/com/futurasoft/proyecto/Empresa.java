762f863e611fe2153edbee574d0e43328ec0e36b
Juan C. Perez <22458962+razielfayad@users.noreply.github.com>
2022-08-27 14:37:02 -0500

    agregado clase Empresa.java

---
+++ src/main/java/com/futurasoft/proyecto/Empresa.java
@@ -0,0 +1,41 @@
+package com.futurasoft.proyecto;
+
+public class Empresa {
+    private String Nombre;
+    private String Direccion;
+    private String Telefono;
+    private String Nit;
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getNit() {
+        return Nit;
+    }
+
+    public void setNit(String nit) {
+        Nit = nit;
+    }
+}
