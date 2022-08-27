d4c4aa4211b0ddf5b25b72d2ec9473cee8b3ec90
Javier <d.javieravila93@gmail.com>
2022-08-27 12:14:10 -0500

    Se agrega clase Empleado

---
+++ src/main/java/com/futurasoft/proyecto/Empleado.java
@@ -0,0 +1,42 @@
+package com.futurasoft.proyecto;
+
+public class Empleado {
+
+    private String nombreEmpleado;
+    private String emailEmpleado;
+    private String empresa;
+    private String rolEmpleado;
+
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getEmailEmpleado() {
+        return emailEmpleado;
+    }
+
+    public void setEmailEmpleado(String emailEmpleado) {
+        this.emailEmpleado = emailEmpleado;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
+}
