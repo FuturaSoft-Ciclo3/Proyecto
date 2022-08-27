1357a810130bc3e05bcb40daf8707aa5544e07d7
Juan C. Perez <22458962+razielfayad@users.noreply.github.com>
2022-08-27 13:36:04 -0500

    agregado getter empleado

--- src/main/java/com/futurasoft/proyecto/MovimientoDinero.java
+++ src/main/java/com/futurasoft/proyecto/MovimientoDinero.java
@@ -5,9 +5,27 @@ public class MovimientoDinero {
     private String ConceptoMovimiento;
     private Empleado Usuario;
 
-    public float getMontoMovimiento() {return MontoMovimiento;}
-    public void setMontoMovimiento(float montoMovimiento) {MontoMovimiento = montoMovimiento;}
-    public String getConceptoMovimiento() {return ConceptoMovimiento;}
-    public void setConceptoMovimiento(String conceptoMovimiento) {ConceptoMovimiento = conceptoMovimiento;}
-    public void setUsuario(Empleado usuario) {Usuario = usuario;}
+    public float getMontoMovimiento() {
+        return MontoMovimiento;
+    }
+
+    public void setMontoMovimiento(float montoMovimiento) {
+        MontoMovimiento = montoMovimiento;
+    }
+
+    public String getConceptoMovimiento() {
+        return ConceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        ConceptoMovimiento = conceptoMovimiento;
+    }
+
+    public Empleado getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(Empleado usuario) {
+        Usuario = usuario;
+    }
 }
